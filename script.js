83ec1ef00822be124fb8e884cdb8bee8b68c2c8f
Victor Daramola <daramolav1@montclair.edu>
2021-01-28 12:14:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let quotesData;
+let colors = [
+  "#d73443",
+  "#6b5ace",
+  "#d36a31",
+  "#265911",
+  "#84754e",
+  "#a7e6fa",
+  "#856189",
+  "#5173a7",
+  "#722f37",
+  "#ed3c3d",
+];
+let currentQuote = "";
+let currentAuthor = "";
+
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: "application/json",
+    },
+    url:
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === "string") {
+        quotesData = JSON.parse(jsonQuotes);
+      }
+    },
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $("#tweet-quote").attr(
+    "href",
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $(".quote-text").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#text").text(randomQuote.quote);
+  });
+
+  $(".quote-author").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#author").html(randomQuote.author);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $("html body").animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color],
+    },
+    1000
+  );
+  $(".button").animate(
+    {
+      backgroundColor: colors[color],
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $("#new-quote").on("click", getQuote);
+});
